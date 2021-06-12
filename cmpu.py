1e2fb3bf034b97ce7e45892099c62bdbaaf677da
gymms <stephan.waser@sbl.ch>
2021-06-12 19:41:33 +0200

    Story 1: Add main program "cmpu.py"

---
+++ cmpu.py
@@ -0,0 +1,15 @@
+# Dies ist die Programmdatei für convertmpu
+#
+def open_mpu():
+    pass
+
+def convert_mpu():
+    pass
+
+def push_tracks_to_excel():
+    pass
+
+if __name__=__main__:
+    open_mpu()
+    convert_mpu()
+    push_tracks_to_excel()
